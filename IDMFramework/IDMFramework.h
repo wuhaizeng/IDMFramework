16f7278a3dcba32fcb7dea8d3a2a27d13e08c940
wuhaizeng <wuhaizeng@cmss.chinamobile.com>
2017-11-21 11:33:34 +0800

    IDMPhotoBrower carthage

---
+++ IDMFramework/IDMFramework.h
@@ -0,0 +1,19 @@
+//
+//  IDMFramework.h
+//  IDMFramework
+//
+//  Created by wuhaizeng on 20/11/2017.
+//  Copyright © 2017 cmss. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for IDMFramework.
+FOUNDATION_EXPORT double IDMFrameworkVersionNumber;
+
+//! Project version string for IDMFramework.
+FOUNDATION_EXPORT const unsigned char IDMFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <IDMFramework/PublicHeader.h>
+
+
